0eb245137c011730a50e03020636753aef93cc88
joeldba <joeldba@github.com>
2021-09-24 12:37:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Prerequisites
+* Xlib headers
+* terminus-font
+
+# Installation
+`cd` into the dmenu directory, then run `sudo make clean install`.
+
+# Patches
+* center
+* lineheight
+* border (set to 2px)
---
+++ config.def.h
@@ -0,0 +1,31 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 300;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"JetBrains Mono:size=8", "FontAwesome5Free:style=Solid:size=8:antialias:true", "FontAwesome5Brands:size=8:antialias:true"
+};
+static const char *prompt      = "run:";      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#81a1c1", "#0f111a" },
+	[SchemeSel] = { "#e5e9f0", "#0f111a" },
+	[SchemeOut] = { "#000000", "#e5e9f0" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 10;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 25;
+static unsigned int min_lineheight = 10;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 15;
